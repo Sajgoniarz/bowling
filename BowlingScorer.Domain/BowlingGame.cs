65223c258466a8456ffe13e5d27480d58ee9268d
Marek Naharnowicz <Sajgoniarz1@gmail.com>
2017-07-27 12:43:17 +0200

    - Add Bowling Game class - Add tests

---
+++ BowlingScorer.Domain/BowlingGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BowlingScorer.Domain
+{
+    public class BowlingGame
+    {
+        public int? Score()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Roll(int pins)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BowlingScorer.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BowlingScorer.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BowlingScorer.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9359bbc9-1d32-4b4c-81e5-a858e18d9ede")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
